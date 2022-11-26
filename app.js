69780f614f94756f4cb1c79513cf8ac22460e218
Cosnavel <42392570+Cosnavel@users.noreply.github.com>
2022-11-26 14:58:13 +0100

    improved route name

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
-app.use('/shoppingLists', shoppingListsRouter)
+app.use('/shopping-lists', shoppingListsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
