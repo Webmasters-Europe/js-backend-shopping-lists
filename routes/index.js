96743a0c6ec0b3bf0880ef324cb9cc21423b0035
Cosnavel <42392570+Cosnavel@users.noreply.github.com>
2022-11-27 23:57:18 +0100

    removed node-fetch and used native fetch

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 const express = require('express')
 
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
-
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-    const userData = await (await fetch('http://localhost:3000/api/909090')).json()
+    const userData = await (await fetch('http://localhost:3000/shoppinglists/909090')).json()
+
     res.render('index', { list: userData.entries })
 })
 
