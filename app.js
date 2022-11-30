8e39cc155761998181bc3c2a3445ff69287e240a
Jonas00016 <juergens.jonas2000@gmail.com>
2022-11-30 11:14:09 +0100

    changed filed according to class content

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-require('dotenv').config()
 const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
-const mongoose = require('mongoose')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
@@ -21,16 +19,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
-
-if (!process.env.DATABASE_URL) {
-    console.error('DATABASE_URL not set')
-    process.exit(1)
-}
-
-mongoose.connect(process.env.DATABASE_URL)
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.error('Database connection successful'))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
