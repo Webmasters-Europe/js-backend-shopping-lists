d60b9bfd6a61945cff79d4e3a48f4aab1cf92011
Cosnavel <42392570+Cosnavel@users.noreply.github.com>
2023-01-28 19:02:38 +0100

    linted

--- config/passport.js
+++ config/passport.js
@@ -2,19 +2,19 @@ const JwtStrategy = require('passport-jwt').Strategy
 const User = require('../models/user')
 
 const options = {
-	jwtFromRequest: req => req?.cookies['bearer'],
-	secretOrKey: process.env.JWT_SECRET,
+    jwtFromRequest: (req) => req?.cookies.bearer,
+    secretOrKey: process.env.JWT_SECRET,
 }
 
-module.exports = passport => {
-	passport.use(
-		new JwtStrategy(options, async (jwt_payload, done) => {
-			const user = await User.findOne({ username: jwt_payload.username })
-			if (user) {
-				return done(null, user)
-			}
+module.exports = (passport) => {
+    passport.use(
+        new JwtStrategy(options, async (jwtPayload, done) => {
+            const user = await User.findOne({ username: jwtPayload.username })
+            if (user) {
+                return done(null, user)
+            }
 
-			return done(null, false)
-		}),
-	)
+            return done(null, false)
+        }),
+    )
 }
