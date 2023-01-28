aa3f1b04529f4fa25109cef5eb403a9ecb78365b
Cosnavel <42392570+Cosnavel@users.noreply.github.com>
2023-01-28 19:32:17 +0100

    fixed logout button

--- public/javascripts/helper/helper.js
+++ public/javascripts/helper/helper.js
@@ -1,14 +1,12 @@
 function registerLogoutHandler() {
-	const logoutButton = Array.from(document.querySelectorAll('.navbar .button')).filter(elem =>
-		elem.innerText.includes('ogout'),
-	)[0]
+    const logoutButton = document.getElementById('Logout')
 
-	logoutButton.addEventListener('click', async () => {
-		const options = {
-			headers: { 'Content-Type': 'application/json' },
-			method: 'POST',
-		}
+    logoutButton.addEventListener('click', async () => {
+        const options = {
+            headers: { 'Content-Type': 'application/json' },
+            method: 'POST',
+        }
 
-		await fetch(`http://localhost:3000/auth/logout`, options)
-	})
+        await fetch('http://localhost:3000/auth/logout', options)
+    })
 }
